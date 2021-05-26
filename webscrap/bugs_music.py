bdbd6e880fd095f4229bf1a1e695fca955bf64bf
pakjkwan <pakjkwan@gmail.com>
2021-05-26 17:28:12 +0900

    17

--- webscrap/bugs_music.py
+++ webscrap/bugs_music.py
@@ -7,45 +7,52 @@ class BugsMusic(object):
     url = 'https://music.bugs.co.kr/chart/track/realtime/total?'
     headers = {'User-Agent': 'Mozilla/5.0'}
     class_name = []
-    title_dict = {}
-
+    title_ls = []
+    artist_ls = []
+    dict = {}
 
     def set_url(self, detail):
         self.url = requests.get(f'{self.url}{detail}', headers=self.headers).text
 
     def get_ranking(self):
         soup = BeautifulSoup(self.url, 'lxml')
-        print('------- 제목 --------')
-        ls = soup.find_all(name='p', attrs=({"class": self.class_name[1]}))
-        for i in ls:
-            print(f' {i.find("a").text}')
-        print('------ 가수 --------')
-        ls = soup.find_all(name='p', attrs=({"class": self.class_name[0]}))
-        for i in ls:
-            print(f'{i.find("a").text}')
+        ls1 = soup.find_all(name='p', attrs=({"class":self.class_name[1]}))
+        ls2 = soup.find_all(name='p', attrs=({"class": self.class_name[0]}))
+        for i in ls1:
+            self.title_ls.append(i.find("a").text)
+        for i in ls2:
+            self.artist_ls.append(i.find("a").text)
 
     def insert_title_dict(self):
-        soup = BeautifulSoup(self.url, 'lxml')
-        print('------- 제목 --------')
-        ls = soup.find_all(name='p', attrs=({"class": self.class_name[1]}))
-        for i in ls:
-            pass
-        print(self.title_dict)
+
+        # 방법 1. range
+        for i in range(0, len(self.title_ls)):
+            self.dict[self.title_ls[i]] = self.artist_ls[i]
+        # 방법 2. zip
+        for i, j in zip(self.title_ls, self.artist_ls):
+            self.title_dict[i] = j
+        # 방법 3. enumerate
+        for i, j in enumerate(self.title_ls):
+            self.dict[self.title_ls[j]] = self.artist_ls[i]
+
+        print(dict)
 
 
     @staticmethod
     def main():
         bugs = BugsMusic()
         while 1:
-            menu = input('0-exit, 1-input time, 2-output')
+            menu = input('0-exit, 1-input time, 2-output, 3-print dict')
             if menu == '0':
                 break
             elif menu == '1':
-                bugs.set_url(input('상세정보 입력')) # wl_ref=M_contents_03_01
+                bugs.set_url('wl_ref=M_contents_03_01')
             elif menu == '2':
                 bugs.class_name.append("artist")
                 bugs.class_name.append("title")
                 bugs.get_ranking()
+            elif menu == '3':
+                bugs.insert_title_dict()
             else:
                 print('Wrong Number')
                 continue
